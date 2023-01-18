f71aef3b86e48e6568b759f8ebfa10e75ca6fce7
DarknessFX <35434089+DarknessFX@users.noreply.github.com>
2023-01-18 13:10:20 -0500

    Add files via upload

--- Util_ShadersPSO.bat
+++ Util_ShadersPSO.bat
@@ -6,8 +6,8 @@ SETLOCAL
 REM CONSTANTS
 SET _RETURN=EXIT /b
 SET _PAUSE=PING -n 4 127.0.0.1^>nul
-SET _ProjectName=
-FOR %%I IN (.) DO SET _ProjectName=%%~nI%%~xI
+SET _PROJECTNAME=
+FOR %%I IN (.) DO SET _PROJECTNAME=%%~nI%%~xI
 
 REM COLOR HELPERS
 SET _bBlack=[40m
@@ -21,7 +21,7 @@ SET _ResetColor=[0m
 REM DATETIME LOG STAMP
 SET _datetime=%date:/=%_%time::=%
 SET _datetime=%_datetime:~0,-3%
-SET LogStamp=%_datetime%
+SET _LOGSTAMP=%_datetime%
 
 REM VARIABLES
 SET _UEUVS=
@@ -36,12 +36,35 @@ ECHO You can read more about PSO cache at :
 ECHO https://docs.unrealengine.com/5.1/en-US/optimizing-rendering-with-pso-caches-in-unreal-engine/
 ECHO.
 ECHO  This process can take a long time! 
+ECHO.
 ECHO  We are going to create a Development build and run it with -logPSO to
 ECHO  generate on demand shaders list, then copy PSO cache from Saved/
 ECHO  CollectedPSOs, convert .upipelinecache and .spk (Stable Shader Key) 
 ECHO  to .spc with ShaderPipelineCacheTools, then package a Development 
 ECHO  build again with PSO cache.
 ECHO.
+%_PAUSE%
+ECHO.
+ECHO  This utility is built for DX12 Deferred Rendering projects, you can 
+ECHO  adapt for Vulkan, but it's pointless to run with DX11 or Forward 
+ECHO  rendering projects.
+ECHO.
+%_PAUSE%
+ECHO.
+ECHO  %_fYellow%%_bBlack%If you're running this utility with your own project, before start 
+ECHO  make sure you edited%_ResetColor%:
+ECHO    \Config\DefaultEngine.ini
+ECHO      [/Script/Engine.RendererSettings]
+ECHO      r.ShaderPipelineCache.Enabled=1
+ECHO      r.ShaderPipelineCache.SaveAfterPSOsLogged=1
+ECHO      [DevOptions.Shaders]
+ECHO      NeedsShaderStableKeys=true
+ECHO    \Config\DefaultGame.ini
+ECHO      [/Script/UnrealEd.ProjectPackagingSettings]
+ECHO      bShareMaterialShaderCode=True
+ECHO      bSharedMaterialNativeLibraries=True
+ECHO.
+ECHO  DFoundryFX_Example project alredy have this configs ready.
 ECHO.
 CALL
 CHOICE /M "Start the utility now?"
@@ -104,10 +127,10 @@ ECHO.
 
 
 :BuildDevelopmentLogPSO
-ECHO %_fCyan%%_bBlack%  - Building %_ProjectName% Development Build .%_ResetColor%
+ECHO %_fCyan%%_bBlack%  - Building %_PROJECTNAME% Development Build .%_ResetColor%
 CALL
 IF EXIST "%CD%\Build\Development\WindowsLogPSO" (
-  ECHO   Found %_ProjectName% Development Build at:
+  ECHO   Found %_PROJECTNAME% Development Build at:
   ECHO     %CD%\Build\Development\WindowsLogPSO.
   ECHO.
   CHOICE /M " Development Build already exist, do you want rebuild?"
@@ -115,10 +138,11 @@ IF EXIST "%CD%\Build\Development\WindowsLogPSO" (
 IF %ERRORLEVEL% NEQ 1 ( %_RETURN% )
 ECHO.
 IF EXIST "%CD%\Build\Development\WindowsLogPSO" ( RMDIR /S /Q "%CD%\Build\Development\WindowsLogPSO" )
-START "Packaging %_ProjectName% - Development Build" /B /WAIT /D "%CD%" CMD /C CALL "%_UEPath%\Build\BatchFiles\RunUAT.bat" -ScriptsForProject="%CD%\%_ProjectName%.uproject" Turnkey -command=VerifySdk -platform=Win64 -UpdateIfNeeded -project="%CD%\%_ProjectName%.uproject" BuildCookRun -nop4 -utf8output -nocompileeditor -skipbuildeditor -cook  -project="%CD%\%_ProjectName%.uproject" -target=%_ProjectName% -unrealexe="%_UEPath%\Binaries\Win64\UnrealEditor-Cmd.exe" -platform=Win64 -stage -archive -package -build -pak -iostore -compressed -prereqs -archivedirectory="%CD%\Build\Development\LogPSO" -clientconfig=Development -nocompile -nocompileuat
+IF EXIST "%CD%\Build\Development\LogPSO" ( RMDIR /S /Q "%CD%\Build\Development\LogPSO" )
+START "Packaging %_PROJECTNAME% - Development Build" /B /WAIT /D "%CD%" CMD /C CALL "%_UEPath%\Build\BatchFiles\RunUAT.bat" -ScriptsForProject="%CD%\%_PROJECTNAME%.uproject" Turnkey -command=VerifySdk -platform=Win64 -UpdateIfNeeded -project="%CD%\%_PROJECTNAME%.uproject" BuildCookRun -nop4 -utf8output -nocompileeditor -skipbuildeditor -cook  -project="%CD%\%_PROJECTNAME%.uproject" -target=%_PROJECTNAME% -unrealexe="%_UEPath%\Binaries\Win64\UnrealEditor-Cmd.exe" -platform=Win64 -stage -archive -package -build -pak -iostore -compressed -prereqs -archivedirectory="%CD%\Build\Development\LogPSO" -clientconfig=Development -nocompile -nocompileuat
 MOVE /Y "%CD%\Build\Development\LogPSO\Windows" "%CD%\Build\Development\WindowsLogPSO"
 IF EXIST "%CD%\Build\Development\LogPSO" ( RMDIR /S /Q "%CD%\Build\Development\LogPSO" )
-ECHO   %_ProjectName% Development Package successfully built.
+ECHO   %_PROJECTNAME% Development Package successfully built.
 ECHO.
 %_PAUSE%
 %_RETURN%
@@ -126,7 +150,7 @@ ECHO.
 
 :StartDevelopmentLogPSO
 ECHO.
-ECHO %_fCyan%%_bBlack%  - Loading %_ProjectName% Development Build .%_ResetColor%
+ECHO %_fCyan%%_bBlack%  - Loading %_PROJECTNAME% Development Build .%_ResetColor%
 ECHO.
 ECHO  Play the game for some seconds just to give time to shaders on demand be 
 ECHO  logged. Check DFoundryFX plugin Shader Tab to verify the shaders on demand 
@@ -136,7 +160,7 @@ ECHO  Exit the game with ALT+F4 .
 ECHO.
 ECHO Starting game in 5 secons.
 TIMEOUT /T 5
-START "Playing %_ProjectName% Development Build" /B /WAIT /D "%CD%\Build\Development\WindowsLogPSO" "%CD%\Build\Development\WindowsLogPSO\%_ProjectName%.exe" -logPSO -ResX=1280 -ResY=720 -WINDOWED -dx12
+START "Playing %_PROJECTNAME% Development Build" /B /WAIT /D "%CD%\Build\Development\WindowsLogPSO" "%CD%\Build\Development\WindowsLogPSO\%_PROJECTNAME%.exe" -logPSO -ResX=1280 -ResY=720 -WINDOWED -dx12
 ECHO   -logPSO gameplay done.
 ECHO.
 %_PAUSE%
@@ -147,11 +171,11 @@ ECHO.
 ECHO %_fCyan%%_bBlack%  - Collecting .upipelinecache and .shk .%_ResetColor%
 ECHO.
 IF NOT EXIST "%CD%\Saved\PSO" ( MKDIR "%CD%\Saved\PSO" )
-START "Copying .upipelinecache" /B /WAIT /D "%CD%" CMD /C XCOPY /S /H /Y /Q "%CD%\Build\Development\WindowsLogPSO\%_ProjectName%\Saved\CollectedPSOs\*.upipelinecache" "%CD%\Saved\PSO\"
-START "Copying .shk" /B /WAIT /D "%CD%" CMD /C XCOPY /S /H /Y /Q "%CD%\Saved\Cooked\Windows\%_ProjectName%\Metadata\PipelineCaches\*SM6.shk" "%CD%\Saved\PSO\"
+START "Copying .upipelinecache" /B /WAIT /D "%CD%" CMD /C XCOPY /S /H /Y /Q "%CD%\Build\Development\WindowsLogPSO\%_PROJECTNAME%\Saved\CollectedPSOs\*.upipelinecache" "%CD%\Saved\PSO\"
+START "Copying .shk" /B /WAIT /D "%CD%" CMD /C XCOPY /S /H /Y /Q "%CD%\Saved\Cooked\Windows\%_PROJECTNAME%\Metadata\PipelineCaches\*SM6.shk" "%CD%\Saved\PSO\"
 ECHO %_fCyan%%_bBlack%  - Expansion .spc .%_ResetColor%
 ECHO.
-START "Expansion .spc" /B /WAIT /D "%_UEPath%" CMD /C CALL "%_UEPath%\Binaries\Win64\UnrealEditor-Cmd.exe" "%CD%\%_ProjectName%.uproject" -run=ShaderPipelineCacheTools expand "%CD%\Saved\PSO\*.upipelinecache" "%CD%\Saved\PSO\*.shk" "%CD%\Saved\PSO\%_ProjectName%_PCD3D_SM6.spc"
+START "Expansion .spc" /B /WAIT /D "%_UEPath%" CMD /C CALL "%_UEPath%\Binaries\Win64\UnrealEditor-Cmd.exe" "%CD%\%_PROJECTNAME%.uproject" -run=ShaderPipelineCacheTools expand "%CD%\Saved\PSO\*.upipelinecache" "%CD%\Saved\PSO\*.shk" "%CD%\Saved\PSO\%_PROJECTNAME%_PCD3D_SM6.spc"
 IF NOT EXIST "%CD%\Build\Windows\PipelineCaches" ( MKDIR "%CD%\Build\Windows\PipelineCaches" )
 START "Copying .spc" /B /WAIT /D "%CD%" CMD /C XCOPY /S /H /Y /Q "%CD%\Saved\PSO\*.spc" "%CD%\Build\Windows\PipelineCaches"
 ECHO.
@@ -162,13 +186,14 @@ ECHO.
 
 
 :BuildDevelopmentPSO
-ECHO %_fCyan%%_bBlack%  - Building %_ProjectName% Development With PSO Build .%_ResetColor%
+ECHO %_fCyan%%_bBlack%  - Building %_PROJECTNAME% Development With PSO Build .%_ResetColor%
 ECHO.
 IF EXIST "%CD%\Build\Development\WindowsWithPSO" ( RMDIR /S /Q "%CD%\Build\Development\WindowsWithPSO" )
-START "Packaging %_ProjectName% - Development Build" /B /WAIT /D "%CD%" CMD /C CALL "%_UEPath%\Build\BatchFiles\RunUAT.bat" -ScriptsForProject="%CD%\%_ProjectName%.uproject" Turnkey -command=VerifySdk -platform=Win64 -UpdateIfNeeded -project="%CD%\%_ProjectName%.uproject" BuildCookRun -nop4 -utf8output -nocompileeditor -skipbuildeditor -cook  -project="%CD%\%_ProjectName%.uproject" -target=%_ProjectName% -unrealexe="%_UEPath%\Binaries\Win64\UnrealEditor-Cmd.exe" -platform=Win64 -stage -archive -package -build -pak -iostore -compressed -prereqs -archivedirectory="%CD%\Build\Development\WithPSO" -clientconfig=Development -nocompile -nocompileuat
+IF EXIST "%CD%\Build\Development\WithPSO" ( RMDIR /S /Q "%CD%\Build\Development\WithPSO" )
+START "Packaging %_PROJECTNAME% - Development Build" /B /WAIT /D "%CD%" CMD /C CALL "%_UEPath%\Build\BatchFiles\RunUAT.bat" -ScriptsForProject="%CD%\%_PROJECTNAME%.uproject" Turnkey -command=VerifySdk -platform=Win64 -UpdateIfNeeded -project="%CD%\%_PROJECTNAME%.uproject" BuildCookRun -nop4 -utf8output -nocompileeditor -skipbuildeditor -cook  -project="%CD%\%_PROJECTNAME%.uproject" -target=%_PROJECTNAME% -unrealexe="%_UEPath%\Binaries\Win64\UnrealEditor-Cmd.exe" -platform=Win64 -stage -archive -package -build -pak -iostore -compressed -prereqs -archivedirectory="%CD%\Build\Development\WithPSO" -clientconfig=Development -nocompile -nocompileuat
 MOVE /Y "%CD%\Build\Development\WithPSO\Windows" "%CD%\Build\Development\WindowsWithPSO"
 IF EXIST "%CD%\Build\Development\WithPSO" ( RMDIR /S /Q "%CD%\Build\Development\WithPSO" )
-ECHO   %_ProjectName% Development Package With PSO successfully built.
+ECHO   %_PROJECTNAME% Development Package With PSO successfully built.
 ECHO.
 %_PAUSE%
 %_RETURN%
@@ -176,17 +201,21 @@ ECHO.
 
 :StartDevelopmentWithPSO
 ECHO.
-ECHO %_fCyan%%_bBlack%  - Loading %_ProjectName% Development With PSO Build .%_ResetColor%
+ECHO %_fCyan%%_bBlack%  - Loading %_PROJECTNAME% Development With PSO Build .%_ResetColor%
 ECHO.
 ECHO  Note that GameWithPSO is going to take longer to start, around 1 minute.
+ECHO.
+ECHO  Read the docs about Controlling PSO Compilation for better control when 
+ECHO  PSO compile in your project.
+ECHO.
 ECHO  Play the game with PSO check DFoundryFX Shader Tab to verify the shaders 
-ECHO  on demand list (around 8 GPU shaders).
+ECHO  on demand list (around 8 GPU shaders in the DFoundryFX_Example project).
 ECHO.
 ECHO  Exit the game with ALT+F4 .
 ECHO.
 ECHO Starting game in 5 secons.
 TIMEOUT /T 5
-START "Playing %_ProjectName% Development With PSO Build" /B /WAIT /D "%CD%\Build\Development\WindowsWithPSO" "%CD%\Build\Development\WindowsWithPSO\%_ProjectName%Game.exe" -logPSO -ResX=1280 -ResY=720 -WINDOWED -dx12
+START "Playing %_PROJECTNAME% Development With PSO Build" /B /WAIT /D "%CD%\Build\Development\WindowsWithPSO" "%CD%\Build\Development\WindowsWithPSO\%_PROJECTNAME%Game.exe" -logPSO -ResX=1280 -ResY=720 -WINDOWED -dx12
 ECHO   -WithPSO gameplay done.
 ECHO.
 %_RETURN%
