a235cd18640bab5ebc9e849046e059a707c439c5
DarknessFX <darkness@fatorx.com.br>
2026-06-19 06:28:56 +0100

    .

--- Source/DFoundryFX_ExampleEditor.Target.cs
+++ Source/DFoundryFX_ExampleEditor.Target.cs
@@ -6,8 +6,8 @@ public class DFoundryFX_ExampleEditorTarget : TargetRules
 	public DFoundryFX_ExampleEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("DFoundryFX_Example");
 	}
 }
--- Source/DFoundryFX_ExampleGame.Target.cs
+++ Source/DFoundryFX_ExampleGame.Target.cs
@@ -6,8 +6,8 @@ public class DFoundryFX_ExampleGameTarget : TargetRules
 	public DFoundryFX_ExampleGameTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("DFoundryFX_Example");
 	}
 }
